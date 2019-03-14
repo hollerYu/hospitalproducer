2ed08ba7541f26487a17f849b5d28d0c1c798e91
liuxingbo521 <liuxingbo9298@dingtalk.com>
2019-03-14 09:44:33 +0800

    刘兴博

--- src/main/java/com/jk/controller/SpecialistTwoController.java
+++ src/main/java/com/jk/controller/SpecialistTwoController.java
@@ -2,6 +2,8 @@ package com.jk.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import com.jk.bean.SpecialistTwo;
+import com.jk.untils.ExportExcel;
+import com.jk.untils.ImportExcel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
@@ -9,14 +11,19 @@ import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class SpecialistTwoController {
     @Autowired
     private MongoTemplate mongoTemplate;
-
+    @Autowired
+    private ServletContext context;
     //跳转到专家页面
     @RequestMapping("zhuanjia")
     public String zhuanjia() {
@@ -54,8 +61,50 @@ public class SpecialistTwoController {
         mongoTemplate.remove(query.addCriteria(Criteria.where("id").in(ids)),specialistTwo.getClass());
         return "success";
     }
-
-
-
+    //poi导出
+    @RequestMapping("exportSpecialist")
+    public void importpoi(HttpServletResponse response) {
+        Query query = new Query();
+        List<SpecialistTwo> list = mongoTemplate.find(query, SpecialistTwo.class);
+        //使用poi将导出
+        String title="专家列表";
+        String[] rowName = {"专家姓名","毕业学院","教师图片","专家分类"};
+        List<Object[]> dataList = new ArrayList<Object[]>();
+        Object[] obj = null;
+        for (SpecialistTwo task : list) {
+            obj = new Object[rowName.length];
+            obj[0] = task.getName();
+            obj[1] = task.getAddress();
+            obj[2] = task.getDataimg();
+            obj[3] = task.getFenlei();
+            dataList.add(obj);
+        }
+        ExportExcel exportExcel = new ExportExcel(title, rowName, dataList, response);
+        try {
+            exportExcel.export();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    //poi导入
+    @ResponseBody
+    @RequestMapping("importExcel")
+    public String importExcel(MultipartFile file) throws Exception{
+        String filePath = file.getOriginalFilename();
+        // System.out.println(filePath);
+        String path = filePath.substring(filePath.lastIndexOf("."));
+        if (path.equals(".xls")) {
+            int startRow = 2;//从表格的哪一行开始读取
+            int endRow = 0;
+            List<SpecialistTwo> bookList = (List<SpecialistTwo>) ImportExcel.importExcel(file.getInputStream(), startRow, endRow, SpecialistTwo.class);
+            for (SpecialistTwo book : bookList) {
+                book.setId(null);
+                mongoTemplate.insert(bookList);
+            }
+            return "1";//成功
+        } else {
+            return "0";
+        }
+    }
 
 }
