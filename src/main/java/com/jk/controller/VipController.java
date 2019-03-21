a6b0c53982c6cffce6f6c5583bebc6bdd050cde6
HaoHuiLin <1091148413@qq.com>
2019-03-21 08:47:43 +0800

    提交

--- src/main/java/com/jk/controller/VipController.java
+++ src/main/java/com/jk/controller/VipController.java
@@ -1,20 +1,13 @@
 package com.jk.controller;
 
-import com.jk.mapper.VipMapper;
 import com.jk.service.VipService;
 import com.jk.untils.ResultPage;
-import net.sf.json.JSONObject;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
 
